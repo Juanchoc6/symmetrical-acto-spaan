20b5d272a2ab1cbfec416537fa205e7ed95a1531
Juanchoc6 <juand.cipamochag@gmail.com>
2024-09-09 19:50:25 -0500

    Se realiza el primer contrato inteligente

---
+++ holaMundo.sol
@@ -0,0 +1,10 @@
+
+//Mi Primer Contrato Hola Mundo 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;   //version de solidity 
+contract holaMundo {
+    string public saludo = "Hola Mundo desde Avalanche Colombia :)";
+    
+}
+
+
